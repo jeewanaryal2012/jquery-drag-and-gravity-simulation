7c3d8f384e4a6240f919d87ef797045c726c3764
jeewanaryal2012 <jeewan.developer@gmail.com>
2016-04-25 00:44:20 -0500

    minor changes

--- drag.php
+++ drag.php
@@ -4,9 +4,9 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width">
 		<title>jQuery Slider - By Jeewan</title>
-		<link rel="stylesheet" type="text/css" href="../css/main.css">
+		<link rel="stylesheet" type="text/css" href="drag.css">
 		<script src="https://code.jquery.com/jquery-1.11.3.js"></script>
-		<script src="../js/drag.js"></script>
+		<script src="drag.js"></script>
 	</head>
 <body>
 
