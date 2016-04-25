51d09ebf3966e0c186199fde12701012fd84e8a3
jeewanaryal2012 <jeewan.developer@gmail.com>
2016-04-25 00:42:05 -0500

    drag php file

---
+++ drag.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title>jQuery Slider - By Jeewan</title>
+		<link rel="stylesheet" type="text/css" href="../css/main.css">
+		<script src="https://code.jquery.com/jquery-1.11.3.js"></script>
+		<script src="../js/drag.js"></script>
+	</head>
+<body>
+
+	<div class="earth">
+		<div class="drag"></div>
+	</div>
+</body>
+</html>
